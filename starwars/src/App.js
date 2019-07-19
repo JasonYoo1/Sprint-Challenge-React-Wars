8b77f124637db4c76be86c9a7fd50366d36a4161
JasonYoo1 <jyoojs@gmail.com>
2019-07-19 09:01:33 -0700

    added Character Name and Height

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -7,24 +7,26 @@ const App = () => {
   const [resultsArray, setresultsArray] = useState([]);
 
   useEffect( () => {
+    // Follow the documentation to learn how to fetch a list of "people"
     axios.get('https://swapi.co/api/people/')
     .then (res => {
       console.log(res.data.results);
+      // Fetch a list of Star Wars characters from the Star Wars API (or SWAPI) and render them to the screen.
       setresultsArray (res.data.results)
+      // Set the data you fetch to state.
     })
   }, [])
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+
+      {/* //used map */}
       {resultsArray.map(card =>{
-        return <CharCard name ={card.name} />
+        return <div className = 'App'>
+          {/* You must have at least one element for each star wars character in the data set. */}
+          <h1>Character Name and Height: </h1><CharCard name ={card.name} height ={card.height} />
+              </div>
       })}
     </div>
   );
--- starwars/src/CharCard.js
+++ starwars/src/CharCard.js
@@ -3,11 +3,12 @@ import React from "react";
 function CharCard(props){
     return(
         <div className='char-card'>
-            <h2>{props.name}</h2>
+            <p>{props.name}</p>
             <p>{props.height}</p>
             <p>{props.mass}</p>
             <p>{props.hair_color}</p>
             <p>{props.skin_color}</p>
+            <img src ={props.url}/>
         </div>
     )
 }
